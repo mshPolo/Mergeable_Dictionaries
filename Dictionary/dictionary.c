0ebf40f0ee2799d4f0de3c665ba87a4570dc4f44
mshPolo <marcus1x@live.dk>
2019-09-01 23:00:46 +0200

    split

--- Dictionary/dictionary.c
+++ Dictionary/dictionary.c
@@ -4,11 +4,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+
 typedef struct {
     int * list;
     int size;
     int used;
 }Dictionary;
+
+
+Dictionary * newDictionary(int size);
+int search(Dictionary * a, int element);
+int findPredecessorIndex(Dictionary * a, int element);
+//INSERT
+void delete(Dictionary * a, int element);
+Dictionary * mergeDictionary(Dictionary * a, Dictionary * b);
+//void splitDictionary(Dictionary * c, int element, Dictionary ** a, Dictionary * b);
 
 Dictionary * newDictionary(int size) {
     Dictionary * a = (Dictionary * ) malloc(sizeof(Dictionary));
@@ -19,12 +30,37 @@ Dictionary * newDictionary(int size) {
 }
 
 int search(Dictionary * a, int element){
-    //TODO
-    return 0;
+    int i = findPredecessorIndex(a,element);
+    return a->list[i];
 }
+
+int findPredecessorIndex(Dictionary * a, int element){
+    int left = 0;
+    int right = a->used-1;
+    int k = 0;
+    while(left < right && k++ < 20){
+            //printf("left: %d\n", left);
+            //printf("right: %d\n", right);
+            int middle = (left + right)/2;
+            //printf("middle: %d\n", middle);
+        if(a->list[middle] < element){
+            left = middle + 1;
+        }else{
+            right = middle;
+        }
+    }
+    if(a->list[left] > element && left > 0){
+        return left-1;
+    }else{
+        return left;
+    }
+}
+
 
 void insertDictionary(Dictionary * list, int element){
     //TODO: if used = size update
+    //TODO: should not insert duplicates
+    //TODO: use binary search
     int i = 0;
     while(i < list->used && list->list[i] < element) i++;
     int temp1  = list->list[i];
@@ -37,30 +73,58 @@ void insertDictionary(Dictionary * list, int element){
     list->used++;
 }
 
+void delete(Dictionary * a, int element){
+    //TODO: if used = size update
+    int i = findPredecessorIndex(a,element);
+    if(a->list[i] != element) return;
+    while(i < a->used){
+         int current = a->list[i+1];
+         a->list[i++] = current;
+    }
+    a->used--;
+}
+
 Dictionary * mergeDictionary(Dictionary * a, Dictionary * b){
-    Dictionary * newList = newDictionary(a->used+b->used);
+    Dictionary * c = newDictionary(a->used+b->used);
     int countA = 0,countB = 0;
+    
     while (countA < a->used && countB < b->used){
         if(a->list[countA] < b->list[countB]){
-            newList->list[countA+countB] = a->list[countA];
+            c->list[countA+countB] = a->list[countA];
             countA++;
         }else{
-            newList->list[countA+countB] = b->list[countB];
+            c->list[countA+countB] = b->list[countB];
             countB++;
         }
     }
-
-    while (countA < a->used){ newList->list[countA+countB] = a->list[countA]; countA++; }
-    while (countB < b->used){ newList->list[countA+countB] = b->list[countB]; countA++; }
     
-    newList->used = a->used + b->used;
+    while (countA < a->used){ c->list[countA+countB] = a->list[countA]; countA++; }
+    while (countB < b->used){ c->list[countA+countB] = b->list[countB]; countB++; }
+    
+    c->used = a->used + b->used;
     //TODO: FREE a AND b
-
-    return newList;
+    
+    return c;
 }
 
-void splitDictionary(Dictionary * c, int e, Dictionary * a, Dictionary * b){
+void splitDictionary(Dictionary * c, int element, Dictionary ** a, Dictionary ** b){
     //TODO
+    int i = findPredecessorIndex(c,element);
+    *a = newDictionary(2 * i);
+    *b = newDictionary(2 * (c->used - i));
+    int j;
+    //Assumes no duplicates
+    for(j = 0; j <= i; j++){
+        (*a)->list[j] = c->list[j];
+        (*a)->used++;
+    }
+    i++;
+    for(j = 0; j < c->used - i; j++){
+        (*b)->list[j] = c->list[j+i];
+        (*b)->used++;
+    }
+    
+    //TODO: FREE c
 }
 
 void printDictionary(Dictionary * list){
--- main.c
+++ main.c
@@ -2,23 +2,47 @@
 #include "Dictionary/dictionary.c"
 
 int main() {
+    
+    //printf("cake\n");
     Dictionary * a = newDictionary(100);
     Dictionary * b = newDictionary(100);
+    Dictionary * a2;
+    Dictionary * a3;
     int i;
-    for(i = 1; i < 50; i += 2){
+    //printf("1");
+    for(i = 0; i < 5000; i += 200){
         insertDictionary(a, i);
     }
-    
-    for(i = 2; i < 50; i += 2){
+    //printf("2");
+    for(i = 100; i < 5000; i += 200){
         insertDictionary(b, i);
     }
+
     
+
+    //printf("3");
     printDictionary(a);
-    printDictionary(b);
+    //delete(a,400);
+    //delete(a,800);
+    //delete(a,900);
+    //printDictionary(a);
+    //printDictionary(b);
 
-    Dictionary * c = mergeDictionary(a,b);
+    printDictionary(a);
+    splitDictionary(a, 1750,&a2,&a3);
 
-    printDictionary(c);
+    printDictionary(a2);
+    printDictionary(a3);
 
+    //mergeDictionary(a,b);
+    //Dictionary * c = mergeDictionary(a,b);
+    //printf("4");
+    //printDictionary(c);
+
+    //printf("%d\n",search(c, 430));
+    //printf("%d\n",search(c, 901));
+    //printf("%d\n",search(c, 4900));
+    
     return 0;
+    
 }
